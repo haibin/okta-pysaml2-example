31097014f6f31f9156c38480bcd5ae8e637cd25d
Liu Haibin <liu.haibin@gmail.com>
2019-02-21 00:15:54 +0800

    Update

--- app.py
+++ app.py
@@ -16,31 +16,13 @@ import logging
 import os
 import uuid
 
-from flask import (
-    Flask,
-    redirect,
-    render_template,
-    request,
-    session,
-    url_for,
-)
-from flask_login import (
-    LoginManager,
-    UserMixin,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
-)
+import flask_login
+import requests
+import saml2
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_bootstrap import Bootstrap
-from saml2 import (
-    BINDING_HTTP_POST,
-    BINDING_HTTP_REDIRECT,
-    entity,
-)
 from saml2.client import Saml2Client
 from saml2.config import Config as Saml2Config
-import requests
 
 # metadata_url_for contains PER APPLICATION configuration settings.
 # Each SAML service that you support will have different values here.
@@ -62,7 +44,7 @@ metadata_url_for = {
 app = Flask(__name__)
 Bootstrap(app)
 app.secret_key = str(uuid.uuid4())  # Replace with your secret key
-login_manager = LoginManager()
+login_manager = flask_login.LoginManager()
 login_manager.setup_app(app)
 logging.basicConfig(level=logging.DEBUG)
 # NOTE:
@@ -80,15 +62,13 @@ def saml_client_for(idp_name=None):
 
     if idp_name not in metadata_url_for:
         raise Exception("Settings for IDP '{}' not found".format(idp_name))
-    acs_url = url_for(
-        "idp_initiated",
-        idp_name=idp_name,
-        _external=True)
-    https_acs_url = url_for(
-        "idp_initiated",
-        idp_name=idp_name,
-        _external=True,
-        _scheme='https')
+
+    # acs: assertion consumer service
+    # url_for takes the view function name
+    # http://localhost:5000/saml/sso/example-okta-com
+    acs_url = url_for("idp_initiated", idp_name=idp_name, _external=True)
+    # https://localhost:5000/saml/sso/example-okta-com
+    https_acs_url = url_for("idp_initiated", idp_name=idp_name, _external=True, _scheme='https')
 
     #   SAML metadata changes very rarely. On a production system,
     #   this data should be cached as approprate for your production system.
@@ -103,10 +83,10 @@ def saml_client_for(idp_name=None):
             'sp': {
                 'endpoints': {
                     'assertion_consumer_service': [
-                        (acs_url, BINDING_HTTP_REDIRECT),
-                        (acs_url, BINDING_HTTP_POST),
-                        (https_acs_url, BINDING_HTTP_REDIRECT),
-                        (https_acs_url, BINDING_HTTP_POST)
+                        (acs_url, saml2.BINDING_HTTP_REDIRECT),
+                        (acs_url, saml2.BINDING_HTTP_POST),
+                        (https_acs_url, saml2.BINDING_HTTP_REDIRECT),
+                        (https_acs_url, saml2.BINDING_HTTP_POST)
                     ],
                 },
                 # Don't verify that the incoming requests originate from us via
@@ -121,14 +101,15 @@ def saml_client_for(idp_name=None):
             },
         },
     }
+
     spConfig = Saml2Config()
     spConfig.load(settings)
     spConfig.allow_unknown_attributes = True
-    saml_client = Saml2Client(config=spConfig)
-    return saml_client
+
+    return Saml2Client(config=spConfig)
 
 
-class User(UserMixin):
+class User(flask_login.UserMixin):
     def __init__(self, user_id):
         user = {}
         self.id = None
@@ -152,13 +133,16 @@ def load_user(user_id):
 def main_page():
     return render_template('main_page.html', idp_dict=metadata_url_for)
 
-
+# ACS Endpoint – Assertion Consumer Service URL – often referred to simply as the SP login URL.
+# This is the endpoint provided by the SP where SAML responses are posted.
+# The SP needs to provide this information to the IDP.
 @app.route("/saml/sso/<idp_name>", methods=['POST'])
 def idp_initiated(idp_name):
     saml_client = saml_client_for(idp_name)
-    authn_response = saml_client.parse_authn_request_response(
-        request.form['SAMLResponse'],
-        entity.BINDING_HTTP_POST)
+
+    # request.form has two fields: SAMLResponse and RelayState
+    authn_response = saml_client.parse_authn_request_response(request.form['SAMLResponse'], saml2.entity.BINDING_HTTP_POST)
+
     authn_response.get_identity()
     user_info = authn_response.get_subject()
     username = user_info.text
@@ -173,26 +157,33 @@ def idp_initiated(idp_name):
             }
     user = User(username)
     session['saml_attributes'] = authn_response.ava
-    login_user(user)
+    flask_login.login_user(user)
     url = url_for('user')
+    print('************** url', url)
     # NOTE:
     #   On a production system, the RelayState MUST be checked
     #   to make sure it doesn't contain dangerous URLs!
     if 'RelayState' in request.form:
+        # 'RelayState' is in the response but it is empty.
         url = request.form['RelayState']
+
+    print('************** url', url)
+    # url is empty so it goes to the home page.
     return redirect(url)
 
 
 @app.route("/saml/login/<idp_name>")
 def sp_initiated(idp_name):
     saml_client = saml_client_for(idp_name)
-    reqid, info = saml_client.prepare_for_authenticate()
+    # Makes all necessary preparations for an authentication request
+    _, info = saml_client.prepare_for_authenticate()
 
     redirect_url = None
     # Select the IdP URL to send the AuthN request to
     for key, value in info['headers']:
         if key is 'Location':
             redirect_url = value
+
     response = redirect(redirect_url, code=302)
     # NOTE:
     #   I realize I _technically_ don't need to set Cache-Control or Pragma:
@@ -207,7 +198,7 @@ def sp_initiated(idp_name):
 
 
 @app.route("/user")
-@login_required
+@flask_login.login_required
 def user():
     return render_template('user.html', session=session)
 
@@ -218,9 +209,9 @@ def error_unauthorized(error):
 
 
 @app.route("/logout")
-@login_required
+@flask_login.login_required
 def logout():
-    logout_user()
+    flask_login.logout_user()
     return redirect(url_for("main_page"))
 
 if __name__ == "__main__":
